b63efeb3b33f56fd8a1a2d5c5abd73a98c7bf99d
Arif Hidayatullah <arif.hidayatullah@mitrais.com>
2025-07-14 18:19:18 +0700

    output parsers

--- ice_breaker.py
+++ ice_breaker.py
@@ -5,6 +5,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import summary_parser
 from third_parties.linkedin import scrape_linkedin_profile
 
 load_dotenv()
@@ -14,10 +15,15 @@ def ice_break_with(name: str):
             given the Linkedin information {information} about a person I want you to create:
             1. A short summary
             2. two interesting facts about them
+            \n{format_instructions}
             """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions(),
+        }
     )
 
     llm = ChatOpenAI(
@@ -25,7 +31,7 @@ def ice_break_with(name: str):
         model=os.getenv("OPENAI_MODEL_NAME"),
     )
 
-    chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | summary_parser
 
     linkedin_profile = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(
